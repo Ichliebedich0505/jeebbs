d7ed756f8aa8cdb7fb9c9eb4c2bd208e0848aae1
ztwang <ztwnag@qq.com>
2018-09-05 10:46:30 +0800

    add .travis.yml

--- build.sh
+++ build.sh
@@ -11,7 +11,9 @@ ENC_MVN_MASTER_PASSWORD=$(./mvnw --encrypt-master-password $MVN_MASTER_PASSWORD 
 echo "ENC_MVN_MASTER_PASSWORD: $ENC_MVN_MASTER_PASSWORD"
 sed -i "s/examplepwd/$ENC_MVN_MASTER_PASSWORD/g" ./.settings-security.xml && mv ./.settings-security.xml ~/.m2/settings-security.xml
 
-read -s -p "password for $DOCKERHUB_USERNAME: " DOCKERHUB_PASSWORD
+read -p "Please input docker hub username: " DOCKERHUB_USERNAME
+export DOCKERHUB_USERNAME
+read -s -p "Please input password for $DOCKERHUB_USERNAME: " DOCKERHUB_PASSWORD
 export DOCKERHUB_PASSWORD=$(./mvnw --encrypt-password $DOCKERHUB_PASSWORD | tail -n +2)
 
 ./mvnw clean deploy -Dmaven.test.skip=true -s ./.travis-settings.xml -P stage
